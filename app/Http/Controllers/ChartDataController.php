2d59cf9cf9a9c6f210c7ff3e1315b962b535f88e
rubel-ice-pust <rubel.pust06@gmail.com>
2023-07-18 00:39:39 +0600

    All File Added

---
+++ app/Http/Controllers/ChartDataController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\ChartDataModel;
+
+class ChartDataController extends Controller
+{
+
+    function onAllSelect(){
+        $result = ChartDataModel::all();
+        return $result;
+
+    }
+}
